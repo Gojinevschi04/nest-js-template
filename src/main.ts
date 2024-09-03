e6818dd30ce8a06ea9cce38448adb4ee64c81de0
Gojinevschi04 <annagojinevschi@gmail.com>
2024-09-03 14:07:54 +0300

    add swagger and update env.example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configSwagger = app.get(ConfigService);
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  if (configSwagger.get<boolean>('ENABLE_OPEN_API')) {
+    SwaggerModule.setup('api', app, document);
+  }
+  const port = configSwagger.get<number>('APP_PORT') as any;
+  await app.listen(port);
 }
+
 bootstrap();
